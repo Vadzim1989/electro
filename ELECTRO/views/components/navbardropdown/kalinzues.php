85bd6549791f6870df0edb86fc7fcc6777958b83
Vadim Denisovski <vadim.denisovski@gmail.com>
2023-10-19 08:57:42 +0300

    delete trash

--- ELECTRO/views/components/navbardropdown/kalinzues.php
+++
@@ -1,7 +0,0 @@
-<div class="dropdownContent">
-    <a class="nav-link active" aria-current="page" href="/ELECTRO/rues?id=42">Светлогорск</a>
-    <a class="nav-link active" aria-current="page" href="/ELECTRO/rues?id=45">Калинковичи</a>
-    <a class="nav-link active" aria-current="page" href="/ELECTRO/rues?id=50">Петриков</a>
-    <a class="nav-link active" aria-current="page" href="/ELECTRO/rues?id=53">Житковичи</a>
-    <a class="nav-link active" aria-current="page" href="/ELECTRO/rues?id=57">Октябрьский</a>
-</div>
--- ELECTRO/views/components/navbardropdown/rechzues.php
+++
@@ -1,6 +0,0 @@
-<div class="dropdownContent">
-    <a class="nav-link active" aria-current="page" href="/ELECTRO/rues?id=40">Речица</a>
-    <a class="nav-link active" aria-current="page" href="/ELECTRO/rues?id=44">Брагин</a>
-    <a class="nav-link active" aria-current="page" href="/ELECTRO/rues?id=46">Хойники</a>
-    <a class="nav-link active" aria-current="page" href="/ELECTRO/rues?id=47">Лоев</a>
-</div>
